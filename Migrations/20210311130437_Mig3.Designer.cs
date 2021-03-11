f59a1cfe74cb19ce28644556ba6aa2061be491fa
filip-naumovski <filip.naumovski00@live.com>
2021-03-11 14:20:30 +0100

    Remove some unneeded code

--- Migrations/20210311130437_Mig3.Designer.cs
+++ Migrations/20210311130437_Mig3.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OWEBAPI.Models;
 
 namespace OWEBAPI.Migrations
 {
     [DbContext(typeof(OWebDBContext))]
-    partial class OWebDBContextModelSnapshot : ModelSnapshot
+    [Migration("20210311130437_Mig3")]
+    partial class Mig3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
